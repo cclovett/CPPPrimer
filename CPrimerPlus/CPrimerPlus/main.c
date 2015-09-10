ff790d18bc3d5eab3492a0f3c5564183bcb9be39
fhr <fenghaoran@camera360.com>
2015-09-10 17:15:46 +0800

    [cc]

---
+++ CPrimerPlus/CPrimerPlus/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CPrimerPlus
+//
+//  Created by Cc on 15/9/10.
+//  Copyright (c) 2015年 PinguoSDK. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
